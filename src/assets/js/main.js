9b5f22934ea8bd3753d9b009e2ab5365fd53736d
ilyoung <ext3fs@hanmail.net>
2024-04-23 04:50:54 +0900

    layout complete

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,11 @@
-window.onload = function () {
-};
+import {menu} from './menu.js';
+import {link} from './link.js';
+import {portfolio} from './portfolio.js';
+import {smooth} from './smooth.js';
+
+window.addEventListener("load", function () {
+  menu();
+  link();
+  portfolio();
+  smooth();
+});
