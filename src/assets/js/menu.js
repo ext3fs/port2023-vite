19ac2deab430bf5e68e7fae3234163cc2d25ef6b
ilyoung <ext3fs@hanmail.net>
2024-04-25 00:57:16 +0900

    header complete

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,15 @@
 export function menu() {
-  console.log(menu);
+  const headerToggle = document.getElementById("headerToggle");
+  const headerNav = document.querySelector(".header__nav");
+
+  if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+      headerNav.classList.toggle("show");
+
+      if (headerToggle.getAttribute("aria-expanded") === "true")
+        headerToggle.setAttribute("aria-expanded", "false");
+      else
+        headerToggle.setAttribute("aria-expanded", "true");
+    });
+  }
 }
