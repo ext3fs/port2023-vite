3f8dcf3730b43c82229cb7b84435f6918eb763e5
ilyoung <ext3fs@hanmail.net>
2024-05-19 02:53:18 +0900

    footer complete

--- src/assets/js/link.js
+++ src/assets/js/link.js
@@ -1,3 +1,13 @@
 export function link() {
-  console.log(link);
+  
+  document.querySelectorAll(".header__nav a[href^='#']").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const addr = this.getAttribute("href");
+      const element = document.querySelector(addr);
+      if(element)
+        element.scrollIntoView({ behavior: "smooth"});
+    });
+  });
+
 }
